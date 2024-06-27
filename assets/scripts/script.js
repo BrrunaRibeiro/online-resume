3e30c061961f5c7ee97147c153ce52932445314a
BrrunaRibeiro <brrunarib@gmail.com>
2024-06-27 16:36:19 +0200

    Add google maps api

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,9 @@
-var API_KEY = 'AIzaSyAY8ICNEvzO_hWEK1HN1_XMfehpXzo4ukk';
+function initMap() {
+    var map = new.google.maps.Map(document.getElementById('map'), {
+        zoom: 3.
+        center: {
+            lat: 41.85,
+            lng: -87.65
+        };
+    });
+};
