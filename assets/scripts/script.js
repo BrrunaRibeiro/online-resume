512b86c1d449a234f6068a3157280e16ccf42adb
BrrunaRibeiro <brrunarib@gmail.com>
2024-07-20 09:54:11 +0200

    Add gitHub APi functionality

--- assets/scripts/script.js
+++
@@ -1,9 +0,0 @@
-function initMap() {
-    var map = new.google.maps.Map(document.getElementById('map'), {
-        zoom: 3.
-        center: {
-            lat: 41.85,
-            lng: -87.65
-        };
-    });
-};
